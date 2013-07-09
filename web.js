50fbd55dc80ff110178535ee1a44bd668342906d
Ryckes <kevin.mnmn@gmail.com>
2013-07-09 08:35:44 +0000

    Second try

--- web.js
+++ web.js
@@ -4,6 +4,6 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html", { encoding: "utf-8" }));
+  response.send(fs.readFileSync("index.html").toString());
   console.log("Listening on " + port);
 });
